494d5282557639a3a1bb03f1a58f5a320d100c84
Aleksandr Lerko <allerk@ttu.ee>
2025-01-29 21:40:41 +0200

    feat: implement search box logic

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,15 +1,40 @@
-import { Component } from 'react';
+import { Component, ReactNode } from 'react';
 import './Main.css';
 import { Header } from '../top-controls/header/Header.tsx';
 import { Search } from '../top-controls/search/Search.tsx';
 import { Results } from '../results/Results.tsx';
+import { CharacterInfo, PaginatedResponse } from '../../domain/IApiResponse.ts';
+import getDataFromApi from '../../services/getDataFromApi.ts';
 
-export class Main extends Component {
-  render() {
+interface IState {
+  results: CharacterInfo[];
+  isLoading: boolean;
+}
+
+export class Main extends Component<unknown, IState> {
+  state = {
+    results: [],
+    isLoading: false,
+  };
+
+  handleSearch = async (searchTerm?: string): Promise<void> => {
+    const data: CharacterInfo[] = await this.fetchData(searchTerm);
+    this.setState({
+      results: data,
+    });
+  };
+
+  fetchData = async (searchTerm?: string): Promise<CharacterInfo[]> => {
+    const response: PaginatedResponse<CharacterInfo> =
+      await getDataFromApi(searchTerm);
+    return response.results;
+  };
+
+  render(): ReactNode {
     return (
       <div className="w-full">
         <Header>
-          <Search />
+          <Search handleSearch={this.handleSearch} />
         </Header>
         <div className="split-line"></div>
         <Results></Results>
