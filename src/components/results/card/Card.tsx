651d50fe5d6d541d7749c73f6350469c890f1260
Aleksandr Lerko <allerk@ttu.ee>
2025-02-03 01:21:08 +0200

    refactor: card component from class to func

--- src/components/results/card/Card.tsx
+++ src/components/results/card/Card.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import picturePlaceholder from '../../../images/picture-placeholder.jpg';
 import './Card.css';
 import { CharacterInfo } from '../../../domain/IApiResponse.ts';
@@ -7,24 +7,23 @@ interface IProps {
   item: CharacterInfo;
 }
 
-export class Card extends Component<IProps> {
-  render(): ReactNode {
-    const { item } = this.props;
-    return (
-      <li className="card">
-        <h2>{item.name}</h2>
-        <div className="card-image">
-          <img src={picturePlaceholder} alt={'404 placeholder'} />
-        </div>
-        <p>
-          The character - <span className="bold-text">{item.name}</span> is{' '}
-          <span className="red-text">{item.height}</span> height and it weighs
-          around <span className="red-text">{item.mass}</span> kgs.{' '}
-          <span className="bold-text">{item.name}</span> identifies as a{' '}
-          <span className="red-text">{item.gender}</span> person and has a{' '}
-          <span className="red-text">{item.eye_color}</span> eyes.
-        </p>
-      </li>
-    );
-  }
-}
+const Card = ({ item }: IProps): ReactNode => {
+  return (
+    <li className="card">
+      <h2>{item.name}</h2>
+      <div className="card-image">
+        <img src={picturePlaceholder} alt={'404 placeholder'} />
+      </div>
+      <p>
+        The character - <span className="bold-text">{item.name}</span> is{' '}
+        <span className="red-text">{item.height}</span> height and it weighs
+        around <span className="red-text">{item.mass}</span> kgs.{' '}
+        <span className="bold-text">{item.name}</span> identifies as a{' '}
+        <span className="red-text">{item.gender}</span> person and has a{' '}
+        <span className="red-text">{item.eye_color}</span> eyes.
+      </p>
+    </li>
+  );
+};
+
+export default Card;
