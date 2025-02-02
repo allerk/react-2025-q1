a2a222fcb2bc471c5f711453ede9e48b6e5a52f8
Aleksandr Lerko <allerk@ttu.ee>
2025-02-03 01:21:27 +0200

    refactor: card LIST component from class to func

--- src/components/results/card-list/CardList.tsx
+++ src/components/results/card-list/CardList.tsx
@@ -1,21 +1,20 @@
-import { Component, ReactNode } from 'react';
-import { Card } from '../card/Card.tsx';
+import { ReactNode } from 'react';
 import './CardList.css';
 import { CharacterInfo } from '../../../domain/IApiResponse.ts';
+import Card from '../card/Card.tsx';
 
 interface IProps {
   results: CharacterInfo[];
 }
 
-export class CardList extends Component<IProps> {
-  render(): ReactNode {
-    const { results } = this.props;
-    return (
-      <ul className="flex justify-center items-stretch flex-wrap">
-        {results.map((item) => (
-          <Card key={item.name} item={item}></Card>
-        ))}
-      </ul>
-    );
-  }
-}
+const CardList = ({ results }: IProps): ReactNode => {
+  return (
+    <ul className="flex justify-center items-stretch flex-wrap">
+      {results.map((item) => (
+        <Card key={item.name} item={item}></Card>
+      ))}
+    </ul>
+  );
+};
+
+export default CardList;
