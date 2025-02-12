c96c12034a49845a8f1f91852033932f48c8f582
Aleksandr Lerko <allerk@ttu.ee>
2025-02-12 22:58:02 +0200

    fix: delete commented code

--- src/common/widgets/loader/Loader.tsx
+++ src/common/widgets/loader/Loader.tsx
@@ -6,21 +6,6 @@ interface IProps {
   children: ReactNode;
 }
 
-// export class Loader extends Component<IProps, unknown> {
-//   render(): ReactNode {
-//     const { isLoading, children } = this.props;
-//     if (isLoading) {
-//       return (
-//         <div className="md:container md:mx-auto flex justify-center h-full items-center">
-//           <div className="loader"></div>
-//         </div>
-//       );
-//     }
-//
-//     return children;
-//   }
-// }
-
 const Loader = ({ isLoading, children }: IProps): ReactNode => {
   if (isLoading) {
     return (
