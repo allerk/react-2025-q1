00e2f91fd9e7ab631d997b598ce561f224fb4305
Aleksandr Lerko <allerk@ttu.ee>
2025-02-12 21:51:16 +0200

    feat: update path to include details route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,7 @@
 import { createRoutesFromElements, Route, RouteObject } from 'react-router';
 import App from '../App.tsx';
 import Main from '../components/main/Main.tsx';
-import Details from '../components/details/Details.tsx';
+import Details from '../components/results/details/Details.tsx';
 
 const routes: RouteObject[] = createRoutesFromElements(
   <Route element={<App />}>
