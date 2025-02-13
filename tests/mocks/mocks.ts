ff6672e1465a05e7ff63eaeb17706539897d768c
Aleksandr Lerko <allerk@ttu.ee>
2025-02-13 15:28:41 +0200

    feat: add mock data for pagination

---
+++ tests/mocks/mocks.ts
@@ -0,0 +1,7 @@
+import { PageInfo } from '../../src/domain/IApiResponse';
+
+export const pageInfo: PageInfo = {
+  next: 'https://swapi.dev/api/people/?page=2',
+  previous: null,
+  totalPages: 9,
+};
