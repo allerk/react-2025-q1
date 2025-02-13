1f0063e264d21d85a31f47634b53344da4268f12
Aleksandr Lerko <allerk@ttu.ee>
2025-02-13 15:02:20 +0200

    feat: add search component tests

--- src/components/top-controls/search/Search.tsx
+++ src/components/top-controls/search/Search.tsx
@@ -34,11 +34,13 @@ const Search = ({ storedValue, handleChange }: IProps): ReactNode => {
           type="text"
           value={searchTerm}
           onChange={handleInputChange}
+          data-testid="searchFormInput"
         />
         <div className="w-1/3 flex justify-center items-center">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
             type="submit"
+            data-testid="submitButton"
           >
             Search
           </button>
