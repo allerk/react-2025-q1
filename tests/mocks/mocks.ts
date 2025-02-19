1deaa1dd134ad6e19ed9a5c8353d30bf5a32b428
Aleksandr Lerko <allerk@ttu.ee>
2025-02-19 21:33:23 +0200

    feat: add mock data file

--- tests/mocks/mocks.ts
+++ tests/mocks/mocks.ts
@@ -1,7 +1,37 @@
-import { PageInfo } from '../../src/domain/IApiResponse';
+import { CharacterInfo, PageInfo } from '../../src/domain/IApiResponse';
+import { IResult } from '../../src/domain/IResults';
 
 export const pageInfo: PageInfo = {
   next: 'https://swapi.dev/api/people/?page=2',
   previous: null,
   totalPages: 9,
+};
+
+export const characterResult: IResult<CharacterInfo> = {
+  responseData: {
+    name: 'Luke Skywalker',
+    height: '172',
+    mass: '77',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    gender: 'male',
+    url: 'https://swapi.dev/api/people/1/',
+  },
+  statusData: {
+    isLoading: false,
+    isStart: false,
+    serverError: null,
+  },
+};
+
+export const characterInfo: CharacterInfo = {
+  name: 'Luke Skywalker',
+  height: '172',
+  mass: '77',
+  hair_color: 'blond',
+  skin_color: 'fair',
+  eye_color: 'blue',
+  gender: 'male',
+  url: 'https://swapi.dev/api/people/1/',
 };
